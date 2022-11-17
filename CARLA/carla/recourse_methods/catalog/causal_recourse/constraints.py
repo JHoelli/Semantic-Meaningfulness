4c296f805fb76f115a18e804064b1ba72cab4de2
Jacqueline Hoellig <hoellig@fzi.de>
2022-11-17 10:59:14 +0100

    some debugging print outs

--- CARLA/carla/recourse_methods/catalog/causal_recourse/constraints.py
+++ CARLA/carla/recourse_methods/catalog/causal_recourse/constraints.py
@@ -26,9 +26,12 @@ def point_constraint(scm, factual_instance, action_set, sampling_handle, mlmodel
     # if action set is empty, return false as we don't flip the label with a factual instance
     if not bool(action_set):
         return False
-
+    #Samples dataset from Original Instance
+    print('Action Set', action_set)
     sampler = Sampler(scm)
     cf_instance = sampler.sample(1, factual_instance, action_set, sampling_handle)
-
+    print('cf Inatance from Constraint', cf_instance)
     prediction = mlmodel.predict(cf_instance)
+    print('prediction from DL Model', prediction)
+    print('Returns', prediction.round() == 1 )
     return prediction.round() == 1
