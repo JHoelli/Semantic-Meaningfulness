3836151087d230e7335ca6cc05a8baee6cae288b
jhoelli <j.hoellig@gmx.net>
2023-01-24 16:24:50 +0100

    added Results and some Plot

--- 2_Summarize_Experiment_Results.py
+++ 2_Summarize_Experiment_Results.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 i=0
+j=0
 for dataset in os.listdir('./Results'):
     if os.path.isdir(f'./Results/{dataset}'):
         for file in os.listdir(f'./Results/{dataset}'):
@@ -12,5 +13,12 @@ for dataset in os.listdir('./Results'):
                     i=i+1
                 else: 
                     df= pd.concat([df,pd.read_csv(f'./Results/{dataset}/{file}') ])
+            elif file.startswith('Res'):
+                if j == 0: 
+                    df_res=pd.read_csv(f'./Results/{dataset}/{file}')
+                    j=j+1
+                else: 
+                    df_res= pd.concat([df_res,pd.read_csv(f'./Results/{dataset}/{file}') ])
 
 df.to_csv('./Results/summary.csv')
+df_res.to_csv('./Results/results.csv')
