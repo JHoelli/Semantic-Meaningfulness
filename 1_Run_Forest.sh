6d298158f125b49306e4ccf172d3334aa9603fc6
jhoelli <j.hoellig@gmx.net>
2023-02-04 09:24:33 +0100

    Forest Results

--- 1_Run_Forest.sh
+++ 1_Run_Forest.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-python 1_Experiment.py sanity-3-lin forest focus 100 both
-python 1_Experiment.py sanity-3-lin forest FeatureTweak 100 both
+python 1_Experiment.py sanity-3-lin forest focus 250 both
+python 1_Experiment.py sanity-3-lin forest FeatureTweak 250 both
 
-python 1_Experiment.py sanity-3-non-lin forest focus 100 both
-python 1_Experiment.py sanity-3-non-lin forest FeatureTweak 100 both
+python 1_Experiment.py sanity-3-non-lin forest focus 250 both
+python 1_Experiment.py sanity-3-non-lin forest FeatureTweak 250 both
 
-python 1_Experiment.py sanity-3-non-add forest focus 100 both
-python 1_Experiment.py sanity-3-non-add forest FeatureTweak 100 both
+python 1_Experiment.py sanity-3-non-add forest focus 250 both
+python 1_Experiment.py sanity-3-non-add forest FeatureTweak 250 both
 
-python 1_Experiment.py credit forest focus 100 both
-python 1_Experiment.py credit forest FeatureTweak 100 both
+python 1_Experiment.py credit forest focus 250 both
+python 1_Experiment.py credit forest FeatureTweak 250 both
 
-python 1_Experiment.py economic forest focus 100 both
-python 1_Experiment.py economic forest FeatureTweak 100 both
+python 1_Experiment.py economic forest focus 250 both
+python 1_Experiment.py economic forest FeatureTweak 250 both
