1c476141f2f4d2c524f31cbcd25a773e9106e6a2
jhoelli <j.hoellig@gmx.net>
2023-02-05 13:17:15 +0100

    Calculation for Venn

--- 2_Summarize_Experiment_Results.py
+++ 2_Summarize_Experiment_Results.py
@@ -1,9 +1,13 @@
 import os 
 import pandas as pd
 
-
+import numpy as np
 i=0
 j=0
+length_complete =0
+valid=0
+smo=0
+smr=0
 for dataset in os.listdir('./Results'):
     if os.path.isdir(f'./Results/{dataset}'):
         for file in os.listdir(f'./Results/{dataset}'):
@@ -16,9 +20,16 @@ for dataset in os.listdir('./Results'):
             elif file.startswith('Res'):
                 if j == 0: 
                     df_res=pd.read_csv(f'./Results/{dataset}/{file}')
+                    valid+=len(df_res)
+                    length_complete+=250
+                    smo+=len(np.where(df_res['semantic']==1))
+                    print(df_res.columns)
+                    smr+=len(np.where(df_res['correct_relationships']==1.0))
+
                     j=j+1
                 else: 
                     df_res= pd.concat([df_res,pd.read_csv(f'./Results/{dataset}/{file}') ])
 
 df.to_csv('./Results/summary.csv')
 df_res.to_csv('./Results/results.csv')
+pd.DataFrame([[valid, length_complete, smo, smr]], columns=['valid', 'length_complete', 'smo', 'smr']).to_csv('./Results/Venn.csv')
